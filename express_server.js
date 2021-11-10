2d5ac86b3563ae19e90d6113aa09e20ba6086ad8
Farida <faridamus@protonmail.com>
2021-11-10 19:06:32 +0000

    Add a GET Route to Show the Form

--- express_server.js
+++ express_server.js
@@ -14,6 +14,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
