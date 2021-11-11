849f69a1a456ae66372bf40564b84de783b1ef20
Farida <faridamus@protonmail.com>
2021-11-11 20:09:55 +0000

    Revise the code and add endpoint to handle POST to login

--- express_server.js
+++ express_server.js
@@ -1,78 +1,83 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
-//Set ejs as the view engine:
-app.set("view engine", "ejs");
-app.use(cookieParser());
-
-const bodyParser = require("body-parser");
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// Implement a function that returns a string of 6 random alphanumeric characters.
-
-function generateRandomString() {
-  return Math.random().toString(36).substr(2,6);
-};
-
-//Get requests:
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
+//Set up GET requests:
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//Add a get route to show the form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-  }
+  const shortURL = req.params.shortURL; 
+  const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL; 
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  const shortString = generateRandomString();
-  urlDatabase[shortString] = req.body.longURL;
-  res.redirect(`/urls/${shortString}`);
-});
-
-// Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// app.get("/", (req, res) => {
+  //   res.send("Hello!");
+  // });
+  
+  // app.get("/hello", (req, res) => {
+    //   res.send("<html><body>Hello <b>World</b></body></html>\n");
+    // });
+    
+    // app.get("/urls.json", (req, res) => {
+      //   res.json(urlDatabase);
+      // });
+      
+      //Set up POST requests:  
+      app.post("/urls", (req, res) => {
+        const shortString = generateRandomString();
+        urlDatabase[shortString] = req.body.longURL;
+        res.redirect(`/urls/${shortString}`);
+      });
+      
+      app.post("/urls/:id", (req, res) => {
+        const shortURL = req.params.id; 
+        const longURL = req.body.newURL;
+        urlDatabase[shortURL] = longURL;
+        res.redirect('/urls');
+      }); 
+      
+      app.post("/urls/:shortURL/delete", (req, res) => {
+        const shortURL = req.params.shortURL;
+        delete urlDatabase[shortURL];
+        res.redirect('/urls');
+      });
+      
+      //Set up endpoint to handle POST to login: 
+      app.post("/login", (req, res) => {
+        res.cookie('username', req.body.username);
+        res.redirect('/urls');
+      }); 
+      
+      //Listener
+      app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}!`);
+      });
+      
+      // Implement a function that returns a string of 6 random alphanumeric characters.
+      function generateRandomString() {
+        return Math.random().toString(36).substr(2,6);
+      };
