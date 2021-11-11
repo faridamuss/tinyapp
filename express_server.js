f8d90ee990ab28597eacf5029debed073a8b80ba
Farida <faridamus@protonmail.com>
2021-11-11 19:00:30 +0000

    Install Cookie-Parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 //Set ejs as the view engine:
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
