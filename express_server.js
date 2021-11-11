995d24312d2d6b5293be25c904db9ce66248cab1
Farida <faridamus@protonmail.com>
2021-11-11 03:02:59 +0000

    Reorganize code and update express_server.js file

--- express_server.js
+++ express_server.js
@@ -1,45 +1,66 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+//Set ejs as the view engine:
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+// Implement a function that returns a string of 6 random alphanumeric characters.
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6);
+};
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+//Get requests:
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Add a get route to show the form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  }
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL; 
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
   const shortString = generateRandomString();
   urlDatabase[shortString] = req.body.longURL;
   res.redirect(`/urls/${shortString}`);
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
-  res.render("urls_show", templateVars);
 });
 
 // Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
@@ -50,22 +71,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-// Generate a Random ShortURL
-// Implement a function that returns a string of 6 random alphanumeric characters.
-
-function generateRandomString() {
-  return Math.random().toString(36).substr(2,6);
-};
